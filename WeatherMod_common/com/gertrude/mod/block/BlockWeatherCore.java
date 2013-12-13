80b8cbefd64a412f0de677cc7fcf0af8673ca959
greiko5 <oilrig33@gmail.com>
2013-12-13 16:58:50 -0500

    commit

--- WeatherMod_common/com/gertrude/mod/block/BlockWeatherCore.java
+++ WeatherMod_common/com/gertrude/mod/block/BlockWeatherCore.java
@@ -1,8 +1,13 @@
 package com.gertrude.mod.block;
 
+import com.gertrude.mod.WeatherMod;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockWeatherCore extends Block{
 
@@ -11,6 +16,15 @@ public class BlockWeatherCore extends Block{
 		this.setCreativeTab(CreativeTabs.tabBlock);
 	}
 	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister)
+	{
+		blockIcon = iconRegister.registerIcon(String.format("%s:%s",WeatherMod.modid.toLowerCase(), getUnwrappedUnloaclizedName(this.getUnlocalizedName())));
+	}
 	
+	protected String getUnwrappedUnloaclizedName( String name)
+	{
+		return name.substring(name.indexOf(".")+1);
+	}
 
 }
