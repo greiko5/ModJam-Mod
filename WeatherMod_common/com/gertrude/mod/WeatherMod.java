4ec442760f10ba77dde37bbe1ade325185969bad
greiko5 <oilrig33@gmail.com>
2013-12-13 14:56:54 -0500

    15 min commit

--- WeatherMod_common/com/gertrude/mod/WeatherMod.java
+++ WeatherMod_common/com/gertrude/mod/WeatherMod.java
@@ -8,8 +8,10 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-@Mod(modid = WeatherMod.modid, name = "Weather Mod", version ="Pre-Alpha v0.0.1")
-	@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
+@Mod(modid = WeatherMod.modid, name = "Weather Mod", version ="Pre-Alpha v0.0.2")//Gertrude<3
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
 public class WeatherMod {
 	
 		
